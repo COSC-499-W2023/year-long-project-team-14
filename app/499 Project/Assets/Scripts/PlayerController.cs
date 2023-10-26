5c822ad7c2da7800ed84d8afb5fea676cffda942
guabo <71463415+guabo@users.noreply.github.com>
2023-10-25 19:11:05 -0700

    movement and aim tests

--- app/499 Project/Assets/Scripts/PlayerController.cs
+++ app/499 Project/Assets/Scripts/PlayerController.cs
@@ -30,6 +30,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask layerDetection;
 
     [HideInInspector] public bool unitTest = false;
+    [HideInInspector] public bool unitTest2 = false;
 
     private void Awake()
     {
@@ -40,46 +41,63 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if(attackCharge < attackChargeMax)
-            attackCharge += Time.deltaTime * attackChargeSpeed;
-
-        if(moveDirection.x != 0 || moveDirection.y != 0){
-            animator.SetFloat("X", moveDirection.x);
-            animator.SetFloat("Y", moveDirection.y);
-
-            animator.SetBool("IsWalking", true);
-        } else {
-            animator.SetBool("IsWalking", false);
-        }
-
-        if(!unitTest)
-            if (playerInput.currentControlScheme == "Keyboard&Mouse")
-            {
-                var mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-                Quaternion rotation = Quaternion.LookRotation(mousePos - playerCenter.transform.position, playerCenter.transform.TransformDirection(Vector3.forward));
-                playerCenter.transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
-            }
-            else
-            {
-                Quaternion rotation = Quaternion.LookRotation(aimDirection, playerCenter.transform.TransformDirection(Vector3.forward));
-                playerCenter.transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
-            }
-
-        lineRenderer.positionCount = 1;
-        lineRenderer.SetPosition(0, gunFollow.transform.position);
-        RaycastHit2D hit = Physics2D.Raycast(gunFollow.transform.position, -gunFollow.up, maxAimDistance, layerDetection);
-
-        lineRenderer.positionCount += 1;
-        if(hit.collider != null)
+        if (!unitTest2)
         {
-            lineRenderer.SetPosition(lineRenderer.positionCount - 1, hit.point);
-        }
+            if (animator != null)
+            {
+                if (attackCharge < attackChargeMax)
+                    attackCharge += Time.deltaTime * attackChargeSpeed;
 
+                if (moveDirection.x != 0 || moveDirection.y != 0)
+                {
+                    animator.SetFloat("X", moveDirection.x);
+                    animator.SetFloat("Y", moveDirection.y);
+
+                    animator.SetBool("IsWalking", true);
+                }
+                else
+                {
+                    animator.SetBool("IsWalking", false);
+                }
+            }
+
+
+
+            if (playerInput != null)
+            {
+                if (!unitTest)
+                    if (playerInput.currentControlScheme == "Keyboard&Mouse")
+                {
+                    var mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+                    Quaternion rotation = Quaternion.LookRotation(mousePos - playerCenter.transform.position, playerCenter.transform.TransformDirection(Vector3.forward));
+                    playerCenter.transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+                }
+                else
+                {
+                    Quaternion rotation = Quaternion.LookRotation(aimDirection, playerCenter.transform.TransformDirection(Vector3.forward));
+                    playerCenter.transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+                }
+
+                lineRenderer.positionCount = 1;
+                lineRenderer.SetPosition(0, gunFollow.transform.position);
+                RaycastHit2D hit = Physics2D.Raycast(gunFollow.transform.position, -gunFollow.up, maxAimDistance, layerDetection);
+
+                lineRenderer.positionCount += 1;
+                if (hit.collider != null)
+                {
+                    lineRenderer.SetPosition(lineRenderer.positionCount - 1, hit.point);
+                }
+
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        if (rb != null)
+        {
+            rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -116,5 +134,26 @@ public class PlayerController : MonoBehaviour
             Destroy(bullet, 30);
         }
     }
+
+    public Vector2 GetMoveDirection()
+    {
+        return moveDirection;
+    }
+
+    public void SetMoveDirection(Vector2 direction)
+    {
+        moveDirection = direction;
+    }
+
+    public Vector2 GetAimDirection()
+    {
+        return aimDirection;
+    }
+
+    public void SetAimDirection(Vector2 direction)
+    {
+        aimDirection = direction;
+    }
+
 
 }
