d48e623e89b228b0782d084acc13ea6c60be7e9f
dpescada <darionpescada@gmail.com>
2023-11-30 18:44:16 -0800

    stopped dead enemies from playing walking animation

--- app/499 Project/Assets/Scripts/EnemyHealthSystem.cs
+++ app/499 Project/Assets/Scripts/EnemyHealthSystem.cs
@@ -7,6 +7,7 @@ public class EnemyHealthSystem : MonoBehaviour
     private Animator animator;
     private Rigidbody2D rb;
     private EnemyAttack ea; 
+    private EnemyMovement1 enemyMovement; 
     public int enemyHealth = 2;
 
     private CircleCollider2D enemyCollider;
@@ -39,6 +40,7 @@ public class EnemyHealthSystem : MonoBehaviour
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         ea = GetComponent<EnemyAttack>();
+        enemyMovement = GetComponent<EnemyMovement1>();
         
     }
      // This function is called when a collision is detected.
@@ -64,11 +66,11 @@ public class EnemyHealthSystem : MonoBehaviour
 
     void Die()
     {
-        rb.bodyType = RigidbodyType2D.Static;
+        enemyMovement.enabled = false;
         ea.enabled = false;
-        animator.SetTrigger("Death");
-
         enemyCollider.enabled = false;
+
+        animator.SetTrigger("Death");
 
         if (portal != null)
         {
