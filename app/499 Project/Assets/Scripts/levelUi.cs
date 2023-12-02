bd610bb419e1918c60785f94c3d13c0dc4eb1283
guabo <71463415+guabo@users.noreply.github.com>
2023-12-01 20:37:34 -0800

    UI now displays level based on current level

---
+++ app/499 Project/Assets/Scripts/levelUi.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class levelUi : MonoBehaviour
+{
+    private Text levelText;
+
+    private void Start()
+    {
+        levelText = GetComponent<Text>();
+
+        if (levelText == null)
+        {
+            Debug.LogError("Text component not found on the 'level' GameObject.");
+        }
+    }
+
+    private void Update()
+    {
+        GameMaster gameMaster = GameObject.FindObjectOfType<GameMaster>();
+        int levelNum = gameMaster.currentLevel;
+        levelText.text = "LEVEL " + levelNum;
+    }
+}
