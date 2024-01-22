8d97d7aa20c55af577f803d74a456d2c3b2cd7c0
guabo <71463415+guabo@users.noreply.github.com>
2024-01-21 16:55:00 -0800

    breakable + moving walls are now their own prefab. Fixed enemycollison with moving walls

--- app/499 Project/Assets/Scripts/movingWall.cs
+++ app/499 Project/Assets/Scripts/movingWall.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class movingWall : MonoBehaviour
 {
-    [SerializeField] private GameObject[] waypoints;
+    [SerializeField] public GameObject[] waypoints;
     private int index = 0;
 
-    [SerializeField] private float speed = 2f;
+    [SerializeField] public float speed = 2f;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
@@ -29,13 +29,17 @@ public class movingWall : MonoBehaviour
             if (enemyHealth != null)
             {
                 enemyHealth.takeDamage();
+                if (enemyHealth.enemyHealth < 1)
+                {
+                    enemyHealth.Die();
+                }
             }
             enemyHealth.animator.SetTrigger("isHit");
         }
     }
 
     // Update is called once per frame
-    private void Update()
+    public void Update()
     {
         if (Vector2.Distance(transform.position, waypoints[index].transform.position) < .01f)
         {
