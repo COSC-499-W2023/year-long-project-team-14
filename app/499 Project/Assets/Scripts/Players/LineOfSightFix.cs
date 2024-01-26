f05a13629029eca920a5cb27be5130fb0598b2ce
dpescada <darionpescada@gmail.com>
2024-01-25 21:48:21 -0800

    fixed enemies and players line of sight so it goes through broken walls and players

--- app/499 Project/Assets/Scripts/Players/LineOfSightFix.cs
+++ app/499 Project/Assets/Scripts/Players/LineOfSightFix.cs
@@ -9,14 +9,14 @@ public class LineOfSightFix : MonoBehaviour
     //Detects if players shoot position is inside a wall
     //Prevents line renderer from clipping through objects and bullets from spawning inside solid objects
     void OnTriggerEnter2D(Collider2D collider) {
-        if(collider.gameObject.CompareTag("Wall"))
+        if(collider.gameObject.CompareTag("Wall") || collider.gameObject.CompareTag("Breakable"))
         {
             playerController.aimingInWall = true;
         }
     }
 
     void OnTriggerExit2D(Collider2D collider) {
-        if(collider.gameObject.CompareTag("Wall"))
+        if(collider.gameObject.CompareTag("Wall") || collider.gameObject.CompareTag("Breakable"))
         {
             playerController.aimingInWall = false;
         }
