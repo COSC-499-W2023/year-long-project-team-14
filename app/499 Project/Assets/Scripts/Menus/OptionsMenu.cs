d28342a1a78fe4e63e8cc018ca838cd50ff046ac
kibelesebnemyildirim <sebnemkibeleyildirim@gmail.com>
2024-03-02 15:34:25 -0800

    added code for the increments and decrements of slider values

--- app/499 Project/Assets/Scripts/Menus/OptionsMenu.cs
+++ app/499 Project/Assets/Scripts/Menus/OptionsMenu.cs
@@ -5,37 +5,41 @@ using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using TMPro;
+
 
 
 public class OptionsMenu : MonoBehaviour
 {
     public GameObject eventSystem;
     public GameObject optionsMenuObject;
-
     public GameObject pauseMenuObject;
+
+    public GameObject mainMenuObject;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    public Slider voiceSlider;
+    public Slider masterSlider;
+
 
 
     void Start()
     {
-        StartCoroutine(SetUpControls());
-        StartCoroutine(SelectMenuButton());
-        
+        eventSystem = GameObject.Find("EventSystem");
+        optionsMenuObject = GameObject.Find("OptionsMenu");
+        pauseMenuObject = GameObject.Find("PauseMenu");
+        mainMenuObject = GameObject.Find("MainMenu");
+        musicSlider = GameObject.Find("MusicSlider").GetComponent<Slider>();
+        sfxSlider = GameObject.Find("SFXSlider").GetComponent<Slider>();
+        voiceSlider = GameObject.Find("VoiceSlider").GetComponent<Slider>();
+        masterSlider = GameObject.Find("MasterSlider").GetComponent<Slider>();
     }
 
-    public void OptionsMenu() {
-        optionsMenu = true;
-        mainMenu = false;
-        optionsMenuObject.SetActive(true); 
-        mainMenuObject.SetActive(false); 
-        SelectButton(musicSlider);
-        StartCoroutine(SelectMenuButton());
-    }
-
-    public void BackToPauseMenu() {
-        optionsMenu = false;
-        optionsMenuObject.SetActive(false); 
-        pauseMenuObject.SetActive(true);
-        StartCoroutine(SelectMenuButton());
+    public void OnEnable() {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.5f);
+        voiceSlider.value = PlayerPrefs.GetFloat("VoiceVolume", 0.5f);
+        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
     }
 
 }
