3257a13bfaab0ddf674b4a8aa9a8ebd1447691da
kibelesebnemyildirim <sebnemkibeleyildirim@gmail.com>
2024-03-02 16:09:18 -0800

    fixed the options menu code and added test for the options menu code, working on the test for the music/sound of the test code

--- app/499 Project/Assets/Scripts/Menus/OptionsMenu.cs
+++ app/499 Project/Assets/Scripts/Menus/OptionsMenu.cs
@@ -5,63 +5,46 @@ using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using TMPro;
 using UnityEngine.Audio;
-
-
 
 
 public class OptionsMenu : MonoBehaviour
 {
-    public GameObject eventSystem;
-    public GameObject optionsMenuObject;
-    public GameObject pauseMenuObject;
+    public AudioMixer audioMixer;
+    public Slider musicVolumeSlider;
+    public Slider voiceVolumeSlider;
+    public Button backButton;
+    public GameObject firstSelected;
+    public GameObject pauseMenu;
 
-    public GameObject mainMenuObject;
-    public Slider musicSlider;
-    public Slider sfxSlider;
-    public Slider voiceSlider;
-    public Slider masterSlider;
-
-
-
-    void Start()
+    private void Start()
     {
-        eventSystem = GameObject.Find("EventSystem");
-        optionsMenuObject = GameObject.Find("OptionsMenu");
-        pauseMenuObject = GameObject.Find("PauseMenu");
-        musicSlider = GameObject.Find("MusicSlider").GetComponent<Slider>();
-        voiceSlider = GameObject.Find("VoiceSlider").GetComponent<Slider>();
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0);
+        voiceVolumeSlider.value = PlayerPrefs.GetFloat("VoiceVolume", 0);
+        backButton.Select();
     }
 
-    public void OnEnable() {
-        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
-        voiceSlider.value = PlayerPrefs.GetFloat("VoiceVolume", 0.5f);
-    }
-
-    public void Update() {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            if (optionsMenuObject.activeSelf) {
-                CloseOptionsMenu();
-            }
-        }
-    }
-
-    public void CloseOptionsMenu() {
-        optionsMenuObject.SetActive(false);
-        pauseMenuObject.SetActive(true);
-        eventSystem.GetComponent<EventSystem>().SetSelectedGameObject(GameObject.Find("OptionsButton"));
-    }
-
-    public void SetMusicVolume(float volume) {
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
         PlayerPrefs.SetFloat("MusicVolume", volume);
-       // AudioManager.instance.SetMusicVolume(volume);
     }
 
-    public void SetVoiceVolume(float volume) {
+    public void SetVoiceVolume(float volume)
+    {
+        audioMixer.SetFloat("VoiceVolume", volume);
         PlayerPrefs.SetFloat("VoiceVolume", volume);
-     // AudioManager.instance.SetVoiceVolume(volume);
     }
 
+    public void SetFirstSelected()
+    {
+        EventSystem.current.SetSelectedGameObject(firstSelected);
+    }
 
+    public void Back()
+    {
+        gameObject.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
 }
+    
