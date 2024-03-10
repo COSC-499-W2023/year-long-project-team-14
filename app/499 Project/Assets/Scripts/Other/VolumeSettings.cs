9d42cf3461683af040fecba48ff4c911542cc779
kibelesebnemyildirim <sebnemkibeleyildirim@gmail.com>
2024-03-10 00:36:17 -0800

    code for retrieving and retaining the volume at the start function

--- app/499 Project/Assets/Scripts/Other/VolumeSettings.cs
+++ app/499 Project/Assets/Scripts/Other/VolumeSettings.cs
@@ -6,9 +6,19 @@ public class VolumeSettings : MonoBehaviour
 {
         [SerializeField] private Slider volumeSlider;
 
+ void Start()
+{
+ if (PlayerPrefs.HasKey("Volume"))
+ {
+playerPrefs.GetFloat("Volume", 1);
+  Load();}
+ else
+ {
+Load(); }
 public void ChangeVolume()
 {
     AudioListener.volume = volumeSlider.value;
+    Save();
 }
 
 private void Load()
