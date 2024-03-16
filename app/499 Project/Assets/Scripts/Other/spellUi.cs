2d317097869c6cbf621c0cd5ee9b5e3555310ad1
dpescada <darionpescada@gmail.com>
2024-03-15 23:47:38 -0700

    added freeze cooldown icon

--- app/499 Project/Assets/Scripts/Other/spellUi.cs
+++ app/499 Project/Assets/Scripts/Other/spellUi.cs
@@ -10,6 +10,7 @@ public class spellUi : MonoBehaviour
     public Sprite lightningSprite;
     public Sprite SeekingSprite;
     public Sprite chadSprite;
+    public Sprite freezeSprite;
     Spells spells;
 
     // Start is called before the first frame update
@@ -23,7 +24,7 @@ public class spellUi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (spells.spellName == "Fireball" || spells.spellName == "Lightning" || spells.spellName == "SeekingOrb" || spells.spellName == "SummonChad")
+        if (spells.spellName == "Fireball" || spells.spellName == "Lightning" || spells.spellName == "SeekingOrb" || spells.spellName == "SummonChad" || spells.spellName == "Freeze")
         {
             image.enabled = true;
 
@@ -36,6 +37,8 @@ public class spellUi : MonoBehaviour
                 image.sprite = SeekingSprite;
             else if (spells.spellName == "SummonChad")
                 image.sprite = chadSprite;
+            else if (spells.spellName == "Freeze")
+                image.sprite = freezeSprite;
 
             // Update the fill amount based on the remaining cooldown
             if (spells.cooldownTimer > 0)
