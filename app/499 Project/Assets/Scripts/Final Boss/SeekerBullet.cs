1370cfd25763c259ca351f707e104279675b0540
dpescada <darionpescada@gmail.com>
2024-03-20 18:30:24 -0700

    created boss bullet prefab and script

---
+++ app/499 Project/Assets/Scripts/Final Boss/SeekerBullet.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class SeekerBullet : MonoBehaviour
+{
+    public bool chasePlayer = false;
+    public Vector2 targetPosition;
+
+    public float movementSpeed = 4;
+    public float nextWaypointDistance = 1;
+    
+    Path path;
+    int currentWaypoint = 0;
+
+    Seeker seeker; 
+    Rigidbody2D rb;
+
+    public GameObject target;
+    public GameObject[] players;
+
+    public GameObject explosionPrefab;
+
+    void Start()
+    {
+        //Get components
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        //Start pathfinding
+        InvokeRepeating("UpdatePath", 0f, 0.1f);
+    }
+
+    void Update()
+    {
+        //Get array of players and find closest one
+        StartCoroutine(FindClosestPlayer());
+        
+        //Update target position
+        if(target != null)
+            SetTargetPosition();
+    }
+
+    void UpdatePath()
+    {
+        if(seeker.IsDone())
+            seeker.StartPath(rb.position, targetPosition, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(chasePlayer)
+        {
+            if(path == null)
+                return;
+
+            //Reached target position
+            if(currentWaypoint >= path.vectorPath.Count)
+                return;
+
+            if(currentWaypoint < path.vectorPath.Count)
+            {
+                //Move the orb
+                Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+                Vector2 force = direction * movementSpeed * 250 * Time.deltaTime;
+                rb.AddForce(force);
+            
+                //Increment waypoint
+                float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+                if(distance < nextWaypointDistance)
+                {
+                    currentWaypoint++;
+                }   
+            }
+        }
+    }
+
+    //If orb collides with enemy... KABOOM!!!
+    void OnCollisionEnter2D(Collision2D collision)
+    {   
+        if(collision.gameObject.CompareTag("Player"))
+        {     
+            healthSystem hs = collision.gameObject.GetComponent<healthSystem>();
+            if(hs != null)
+                hs.takeDamage();
+            Explode();
+        }
+
+        if(collision.gameObject.CompareTag("Wall"))
+        {     
+            chasePlayer = true;
+        }
+    }
+
+    //Find random enemy and sets them as the target
+    public IEnumerator FindClosestPlayer() 
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        List<GameObject> alivePlayers = new List<GameObject>();
+
+        for(int i = 0; i < players.Length; i++)
+        {
+            healthSystem hs = players[i].GetComponent<healthSystem>();
+            if(hs != null)
+            {
+                if(hs.life > 0)
+                    alivePlayers.Add(players[i]);
+            }
+        }
+
+        if(alivePlayers.Count > 1)
+        {
+            float distance1 = Vector3.Distance(gameObject.transform.position, alivePlayers[0].transform.position);
+            float distance2 = Vector3.Distance(gameObject.transform.position, alivePlayers[1].transform.position);
+            if(distance2 < distance1)
+                target = alivePlayers[1];
+            else
+                target = alivePlayers[0];
+        }
+        else
+            target = alivePlayers[0];
+        
+        yield return new WaitForSeconds(0.25f);
+        if(target == null)
+            Explode();
+
+        
+    }
+
+    //Sets the pathfinding target position to the target enemies position
+    public void SetTargetPosition() 
+    {
+        healthSystem hs = target.GetComponent<healthSystem>();
+        if(hs != null)
+        {
+            if(hs.life > 0)
+            {
+                targetPosition.x = target.transform.position.x;
+                targetPosition.y = target.transform.position.y;
+            }
+            else if(target != null) Explode();
+        }
+    }
+
+    public void Explode()
+    {
+        //GameObject dashSmoke2 = Instantiate(explosionPrefab, transform.position, transform.rotation);
+        //Destroy(dashSmoke2, 0.55f);
+        Destroy(gameObject);
+    }
+}
