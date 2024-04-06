3f7b74006e609ae91ab6012385bf407aa8fe1a5a
dpescada <darionpescada@gmail.com>
2024-04-06 16:40:58 -0700

    rebalanced some spells

--- app/499 Project/Assets/Scripts/Enemies/EnemyHealthSystem.cs
+++ app/499 Project/Assets/Scripts/Enemies/EnemyHealthSystem.cs
@@ -86,11 +86,11 @@ public class EnemyHealthSystem : MonoBehaviour
     {
         if(collider.gameObject.CompareTag("FireballExplosion"))
         {
-            takeDamage(3);
+            takeDamage(4);
         }
         else if(collider.gameObject.CompareTag("VoidBeam"))
         {
-            takeDamage(4);
+            takeDamage(5);
         }
     }
 
